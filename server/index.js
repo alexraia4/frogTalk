aa86e6e9729d7f8513926f5eaeb03001885752f5
Alexander Raia <alexander.raia@woodsidehomes.com>
2022-10-08 23:18:24 -0600

    ...

--- server/index.js
+++ server/index.js
@@ -1,6 +1,31 @@
 const express = require('express');
 const app = express();
 const cors = require('cors')
+const { Sequelize } = require('sequelize');
+const sqlite3 = require('sqlite3').verbose();
+
+
+
+const selectTest = () => {
+    let pond = new sqlite3.Database('./pond.db', (err) => {
+        if (err) return console.error(err.message);
+        console.log("You're in the pond");
+    });
+
+    // let sql = 'CREATE TABLE IF NOT EXISTS Accounts (accountName TEXT NOT NULL, accountPassword TEXT NOT NULL);'
+    // pond.exec(sql);
+
+    sql = 'INSERT INTO Accounts VALUES("King Frog", "pOOp");';
+    pond.exec(sql);
+
+    sql = 'SELECT * FROM Accounts;'
+    pond.all(sql, [], (err, rows) => {
+        if (err) throw err;
+        rows.forEach((row) => {console.log(row);});
+    });
+
+    pond.close(() => {console.log("You're out of the pond")});
+}
 
 app.use(cors({
     credentials: true
@@ -8,11 +33,7 @@ app.use(cors({
 
 app.get("/derp", ()=>{console.log('FROGGIN THE BACKEND BRUH')});
 
-// const path = require('path');
-// app.use(express.static(__dirname + '/../build'))
-// app.get('*', (req, res) => {
-//      res.sendFile(path.join(__dirname, '../build/index.html')) 
-// })
+app.get("/selecttest", selectTest);
 
 app.listen(42069, () => {
       console.log(`Listening on port 42069`)

36a53d67af34148fa4a07146f54e48d007c1d86d
Alexander Raia <alexander.raia@woodsidehomes.com>
2022-11-30 15:40:01 -0700

    ...

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,11 @@
 import {useState} from 'react';
+import {useHistory} from 'react-router-dom'
 import Axios from 'axios'
 
 export default function Login() {
     const [userName, setUserName] = useState('');
     const [password, setPassword] = useState('');
+    let history = useHistory();
 
     const login = () => {
         Axios.get('http://localhost:42069/derp')
@@ -13,6 +15,7 @@ export default function Login() {
             User Name: <input type="text" onChange={e =>{setUserName(e.target.value)}}/> <br/>
             Password:  <input type="text" onChange={e =>{setPassword(e.target.value)}}/> <br/>
             <div onClick={login}>Login</div>
+            <div onClick={()=>{history.pushState('/createaccount')}}>New User</div>
         </div>
     );
 }
